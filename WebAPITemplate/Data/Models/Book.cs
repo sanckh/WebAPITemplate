98b6bf68bf6639c4c703fded9270d97e8c2af5fe
sanckh <csutton@student.fullsail.edu>
2022-11-04 15:48:01 -0500

    Added first model class

---
+++ WebAPITemplate/Data/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿namespace WebAPITemplate.Data.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string Genre { get; set; }
+        public bool isRead { get; set; }
+        public DateTime? DateRead { get; set; }
+        public int? Rating { get; set; }
+        public string CoverUrl { get; set; }
+        public DateTime DateAdded { get; set; }
+
+
+
+    }
+}
