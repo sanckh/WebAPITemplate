228087449154a02e97c40dd6453dac3a1d0d6097
sanckh <csutton@student.fullsail.edu>
2022-11-05 11:19:06 -0500

    created db connection, added db initializer, and changed placeholder connection string for real one in appsettings. also performed first migration

---
+++ WebAPITemplate/Data/AppDbInitializer.cs
@@ -0,0 +1,32 @@
+﻿using WebAPITemplate.Data.Models;
+
+namespace WebAPITemplate.Data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+
+                if (!context.Books.Any())
+                {
+                    context.Books.AddRange(new Book()
+                    {
+                        Title = "Go Ask Alice",
+                        Author = "Anonymous",
+                        Description = "The harrowing true story of a teenager's descent into the seductive world of drugs.",
+                        Genre = "Fiction",
+                        isRead = true,
+                        DateRead = DateTime.Now,
+                        Rating = 4,
+                        CoverUrl = "Placeholder",
+                        DateAdded = DateTime.Now
+                        
+                    });
+                }
+            }
+        }
+    }
+}
